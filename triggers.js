87f1c68cd60111b4fc2ab1e7332cbc9783a5f6cf
Mitchell Philipp <mgpalpha1@gmail.com>
2023-02-02 17:56:59 -0500

    attempt to add vr shuffle button

--- triggers.js
+++ triggers.js
@@ -1,7 +1,19 @@
 document.addEventListener('keydown', function(event) {
     console.log(event.key);
     if (event.key == "e") {
-        let blocks = document.querySelectorAll("[puzzle-block]");
+        explode();
+    }
+});
+
+// document.addEventListener('buttondown', function(event) {
+//     // console.log(event.key);
+//     // if (event.button == "a") {
+//         explode();
+//     // }
+// });
+
+function explode() {
+    let blocks = document.querySelectorAll("[puzzle-block]");
         for (let block of blocks) {
             console.log("Testing e trigger");
             block.components["puzzle-block"].preExplode();
@@ -9,7 +21,16 @@ document.addEventListener('keydown', function(event) {
         for (let row of gridSingleton.fillGrid) {
             row.fill(false);
         }
+}
+
+AFRAME.registerComponent('explode-listener', {
+    init: function () {
+      var el = this.el;
+      el.addEventListener('abuttondown', explode);
+      el.addEventListener('bbuttondown', explode);
+      el.addEventListener('xbuttondown', explode);
+      el.addEventListener('ybuttondown', explode);
     }
-});
+  });
 
 var lerp = (a,b,t) => a+(b-a)*t;
